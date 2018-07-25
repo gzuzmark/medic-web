6c95b9f4c4957253f141f7fc44bca001ec1e7cc7
Carlos Huamani <carlos.hs.92@gmail.com>
2018-07-24 23:45:54 -0500

    chore(constants): update text constant

--- src/components/ScheduleSession/ScheduleSession.constants.ts
+++ src/components/ScheduleSession/ScheduleSession.constants.ts
@@ -2,7 +2,7 @@ export const SESSION_SKILL = 'skills';
 export const SESSION_TYPE = 'type';
 export const SESSION_SITE = 'site';
 export const SESSION_ROOM = 'room';
-export const SESSION_MAX_STUDENTS = 'SESSION_ROOM';
+export const SESSION_MAX_STUDENTS = 'max_students';
 
 export const SUNDAY = 1;
 export const MONDAY = 2;
