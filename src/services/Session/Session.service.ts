c93afffa03f5b69133f38013e066fe8ab05874b4
Mauricio <mauricio.castaneda@cision.com>
2020-06-14 20:33:28 -0500

    Added schedule follow up session to session list

--- src/services/Session/Session.service.ts
+++ src/services/Session/Session.service.ts
@@ -46,6 +46,23 @@ class SessionService extends BaseRequest {
         });
     }
 
+    public followupSession(oldSessionId: string, newSessionId: string) {
+      return new Promise((resolve, reject) => {
+        this.instance.post(`ugo-admin/session/${oldSessionId}/follow_up?new_session=${newSessionId}`)
+          .then((response: any) => {
+              if (response.status === 200 && response.data) {
+                  resolve(response.data);
+              } else {
+                  reject(null);
+              }
+          })
+          .catch((error: any) => {
+              this.validSession();
+              reject(error);
+          });
+      });
+    }
+
     public getSessionsByDoctor(session: string, doctorId: string, limit?: number) {
         return new Promise((resolve, reject) => {
             const params = `?doctor=${doctorId}&limit=${limit || 10}`;
