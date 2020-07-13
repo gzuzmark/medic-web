71c456e219506401666c144fa29ff89a8b249da6
Christian Bedón <christian.bedonv@gmail.com>
2020-07-13 16:22:00 -0500

    Hotfix change url for electronic prescription

--- src/services/Mentor/Mentor.service.ts
+++ src/services/Mentor/Mentor.service.ts
@@ -5,8 +5,8 @@ import {IMentorEditParams} from "../../domain/Mentor/MentorEditProfile";
 import {IMentorSession} from '../../interfaces/Mentor.interface';
 import BaseRequest from '../BaseRequest';
 
-const GET_ACTIVE_PRINCIPLES_ENDPOINT = 'https://masterproductreci02.backend.cindibyinkafarma.com/activePrinciples';
-const GET_PRODUCT_INFO_ENDPOINT = 'https://masterproductreci02.backend.cindibyinkafarma.com/product';
+const GET_ACTIVE_PRINCIPLES_ENDPOINT = 'https://masterproductre-ci02.cindibyinkafarma.com/activePrinciples';
+const GET_PRODUCT_INFO_ENDPOINT = 'https://masterproductre-ci02.cindibyinkafarma.com/product';
 
 class MentorService extends BaseRequest {
     private verifyMenorCancelToken: any = null;
