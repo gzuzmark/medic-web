4e5f5a26688b5e0df90de9ec8a9334d6160f5f0e
Christian Bedón <christian.bedonv@gmail.com>
2020-11-16 16:01:53 -0500

    Change to psycho duration and nextsession time

--- src/components/Admin/ScheduleSession/components/ScheduleSessionForm/components/WeekendPicker/WeekendPicker.tsx
+++ src/components/Admin/ScheduleSession/components/ScheduleSessionForm/components/WeekendPicker/WeekendPicker.tsx
@@ -72,9 +72,9 @@ class WeekendPicker extends React.Component <IPropsWeekendPicker, IStateWeekendP
 
     private _renderBody(session: FactorySessionBean): JSX.Element[] {
         const firstSession = 420;
-        const nextSession = session.factorySession.skillId === '1' ? 30 : 20;
+        const nextSession = session.factorySession.skillId === '1f541097-dc70-4852-a31a-f2956ab4fab4' ? 30 : 20;
         const lastSession = 1380;
-        const duration = session.factorySession.skillId === '1' ? 20 : 15;
+        const duration = session.factorySession.skillId === '1f541097-dc70-4852-a31a-f2956ab4fab4' ? 30 : 15;
         const rows = [];
         for(let timer = firstSession; timer <= lastSession; timer += nextSession) {
             rows.push(
