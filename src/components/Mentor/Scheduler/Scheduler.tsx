8b37f733e10e96edf23685a0f6ede52ef0ccc076
Jonathan David Gutierrez Matos <jdavidgutierrezm@gmail.com>
2021-05-14 18:47:48 -0500

    create calendar fix error

--- src/components/Mentor/Scheduler/Scheduler.tsx
+++ src/components/Mentor/Scheduler/Scheduler.tsx
@@ -1,22 +1,22 @@
-import * as React from 'react';
+// tslint:disable:ordered-imports
+import { L10n } from '@syncfusion/ej2-base';
 // tslint:disable:ordered-imports
 import {
 	Day,
 	Inject,
 	Month,
 	ScheduleComponent,
-	Week,
-	ViewsDirective,
-	ViewDirective,
+	ViewDirective, ViewsDirective, Week
 } from '@syncfusion/ej2-react-schedule';
-import { localeTranslations } from './locale';
-// tslint:disable:ordered-imports
-import { L10n } from '@syncfusion/ej2-base';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import LayoutContext from '../../../common/Layout/Layout.context';
 
 import Loader from '../../../common/Loader/Loader';
 import { Headline1 } from '../../../common/MentorText';
 import MentorService from '../../../services/Mentor/Mentor.service';
+import MessageService from './components/MessageServices/MessageService';
+import { localeTranslations } from './locale';
 import './Scheduler.scss';
 import { Link } from 'react-router-dom';
 
@@ -53,9 +53,10 @@ const Scheduler = () => {
 	let scheduleObj: any = React.useRef();
 	const mentorService = new MentorService();
 	const [loading, setLoading] = React.useState(false);
-	// const [isServiceExecute, setIsServiceExecute] = React.useState(false);
 	const [appointments, setAppointments] = React.useState<IAppointments[]>([]);
 	const [skills, setSkills] = React.useState<any[]>([]);
+	const [executeService, setExecuteService] = React.useState(false);
+
 	const onPopUpOpen = (args: any) => {
 		if (args.type === 'Editor') {
 			// removing unnecessary fields
@@ -79,9 +80,22 @@ const Scheduler = () => {
 					querySelector.style.display = 'none'
 				};
 			});
+			const selector = args.element.querySelector('.e-schedule-form.e-lib.e-formvalidator');
+			if (selector) {
+				const father = selector.parentElement;
+				const html = (
+					<div className="calendar-create-container">
+						<h4 className="calendar-create-title">Registrar en calendario</h4>
+						<p>Recuerde que esta opción es irreversible</p>
+						<p>Después de crear no podrá eliminar ni editar</p>
+						<p>Verifique que la fecha y hora sean las correctas</p>
+					</div>
+				);
+				ReactDOM.render(html, father);
+			}
 		} else if (args.type === 'EditEventInfo') {
-			// removing unnecessary fields
-			const elements = ['.e-delete.e-icons.e-control'];
+			// removing close button and edit button
+			const elements = ['.e-delete.e-icons.e-control', '.e-edit.e-icons.e-control'];
 			elements.forEach((sel) => {
 				const querySelector = args.element.querySelector(sel);
 				if (querySelector) {
@@ -92,49 +106,51 @@ const Scheduler = () => {
 	}
 
 	const fillSessionsInCalendar = () => {
-		if (skills && skills.length > 0) {
-			const skillId = skills[0].id;
-			const date = new Date();
-			const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
-			const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0, 23);
-			mentorService
-				.getSchedules(skillId, firstDay.toISOString(), lastDay.toISOString())
-				.then((response) => {
-					const schedules = response.items.map(
-						(item: any) => ({
-							Id: item.id,
-							Subject: `${item.doctor_name
-								} ${item.doctor_last_name
-								}`,
-							Subsubject:
-								item.patient_name &&
-									user.id === item.user_id &&
-									item.patient_name
-									? `Paciente: ${item.patient_name
-									} ${item.patient_last_name
-									} `
-									: 'Sin paciente asignado',
-							StartTime: new Date(item.from),
-							EndTime: new Date(item.to),
-							IsReadonly:
-								item.doctor_id !==
-								user.rolId ||
-								!isDateValid(
-									new Date(item.from)
-								),
-							HasPatient:
-								item.patient_name &&
-								user.id === item.user_id,
-							Doctor: item.doctor_id,
-							User: user.id,
-							SessionId: item.id,
-							Description: "fdsf"
-						})
-					);
-					setAppointments(schedules);
-					setLoading(false);
-				});
-		}
+		return new Promise((resolve: any) => {
+			if (skills && skills.length > 0) {
+				const skillId = skills[0].id;
+				const date = new Date();
+				const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+				const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0, 23);
+				mentorService
+					.getSchedules(skillId, firstDay.toISOString(), lastDay.toISOString())
+					.then((response) => {
+						const schedules = response.items.map(
+							(item: any) => ({
+								Id: item.id,
+								Subject: `${item.doctor_name
+									} ${item.doctor_last_name
+									}`,
+								Subsubject:
+									item.patient_name &&
+										user.id === item.user_id &&
+										item.patient_name
+										? `Paciente: ${item.patient_name
+										} ${item.patient_last_name
+										} `
+										: 'Sin paciente asignado',
+								StartTime: new Date(item.from),
+								EndTime: new Date(item.to),
+								IsReadonly:
+									item.doctor_id !==
+									user.rolId ||
+									!isDateValid(
+										new Date(item.from)
+									),
+								HasPatient:
+									item.patient_name &&
+									user.id === item.user_id,
+								Doctor: item.doctor_id,
+								User: user.id,
+								SessionId: item.id,
+								Description: "fdsf"
+							})
+						);
+						setAppointments(schedules);
+						setLoading(false);
+					});
+			}
+		});
 	}
 
 	React.useEffect(() => {
@@ -145,7 +161,8 @@ const Scheduler = () => {
 	}, []);
 
 	React.useEffect(() => {
-		fillSessionsInCalendar();
+		fillSessionsInCalendar()
+			.then(() => setLoading(false));
 	}, [skills]);
 
 	const onCellClick = (args: any) => {
@@ -172,6 +189,7 @@ const Scheduler = () => {
 
 	const onComplete = (args: any) => {
 		if (args.requestType === 'eventCreated') {
+			setExecuteService(true)
 			const { data } = args;
 			const created = (data && data.length > 0 && data[0]) || {};
 			const startTime: Date = created && (created.StartTime as Date);
@@ -192,6 +210,8 @@ const Scheduler = () => {
 				type: 'VIRTUAL',
 			};
 			mentorService.createSessionBulk(bulk).then((response) => {
+				fillSessionsInCalendar()
+					.then(() => setExecuteService(false))
 				return;
 			});
 		} else if (args.requestType === 'eventRemoved') {
@@ -375,6 +395,7 @@ const Scheduler = () => {
 					</ScheduleComponent>
 				)}
 			</div>
+			<MessageService show={executeService} />
 		</div>
 	);
 };
