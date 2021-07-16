db7697780cf07157d51ffd1aae0db32801c24712
gzuzmark <alonso.m144@.com>
2021-07-16 10:31:06 -0500

    Reload page when child recetamedica window is closed.
    TODO: We still need to make the parent window reload just when we are in the 4 step of receta medica

--- src/components/Mentor/Prescription/Prescription.tsx
+++ src/components/Mentor/Prescription/Prescription.tsx
@@ -1,29 +1,58 @@
-import * as React from 'react'
-import Iframe from 'react-iframe';
-import { LOCAL_STORAGE_PRESCRIPTION_URL } from '../SessionsMentor/components/HistorySessions/Constants';
+import * as React from "react";
+import Iframe from "react-iframe";
+import { LOCAL_STORAGE_PRESCRIPTION_URL } from "../SessionsMentor/components/HistorySessions/Constants";
 
-class Prescription extends React.Component  <{}, {}>  {
+class Prescription extends React.Component<{}, {}> {
+    constructor(props: any) {
+        super(props);
+        this.handleWindowClose = this.handleWindowClose.bind(this);
+    }
 
+    public componentDidMount() {
+        window.addEventListener("beforeunload", this.handleWindowClose);
+    }
 
+    public componentWillUnmount() {
+        window.removeEventListener("beforeunload", this.handleWindowClose);
+    }
     public render() {
         // https://web-recetas-electronica-ci05.cindibyinkafarma.com/create/draft-beb04161-988a-40ca-a9ac-4284a02a85bd-298473900074
-        const urlAppoiment = localStorage.getItem(LOCAL_STORAGE_PRESCRIPTION_URL);
-        if(urlAppoiment === null || urlAppoiment === undefined) {
-            return (<div>No encontrado</div>);
+        const urlAppoiment = localStorage.getItem(
+            LOCAL_STORAGE_PRESCRIPTION_URL
+        );
+        if (urlAppoiment === null || urlAppoiment === undefined) {
+            return <div>No encontrado</div>;
         }
-        return(
-            <div style={{textAlign: "center"}}>
-                <Iframe url={urlAppoiment}
+        return (
+            <div style={{ textAlign: "center" }}>
+                <Iframe
+                    url={urlAppoiment}
                     width="100%"
                     height="100%"
                     id="myId"
                     className="myClassname"
                     position="absolute"
-                    />
+                />
             </div>
-        )
+        );
     }
 
+    private handleWindowClose(e: any) {
+        
+        // if (window && window.parent) {
+            // tslint:disable:no-console
+            console.log("we have message sending here parent", window.parent);
+
+            window.opener ? window.opener.postMessage("messagereceipe", window.location.origin): window.parent.postMessage("messagereceipe", window.location.origin);
+            // window.opener?.postMessage('Message to the parent', "*");
+        // }
+        const message = "o/";
+        // tslint:disable:no-console
+        console.log("we have message sending here", { window });
+        console.log("we have message sending here", { parent: window.parent });
+        // (e || window.event).returnValue = message; // Gecko + IE
+        return message;
+    }
 }
 
-export default Prescription
+export default Prescription;
